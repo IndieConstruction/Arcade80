5a81eb4b92de9f9c05bf9e81c677520945ae1e46
Brago <bragolab@gmail.com>
2017-09-27 19:07:03 +0200

    dopo 7200 secondi.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float MovementSpeed = 0.03f;
+    public float JumpForce = 150f;
+    public int CurrentState = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A)) {
+            // left
+            transform.position -= new Vector3(MovementSpeed, 0, 0);
+            CurrentState = 1;
+        } else if (Input.GetKey(KeyCode.D)) {
+            // right
+            transform.position += new Vector3(MovementSpeed, 0, 0);
+            CurrentState = 2;
+        } else {
+            CurrentState = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.O)) {
+            Debug.Log("Salto");
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpForce));
+        }
+
+        GetComponentInChildren<Animator>().SetInteger("State", CurrentState);
+	}
+}
